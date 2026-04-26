7211f6eeffe8ef318247af2c20e2d28229c87dfd
UlianaPythonBackendDeveloper <Uliana_Python_dev@hotmail.com>
2026-04-26 12:25:49 +0300

    add diff_bulder

--- gendiff/scripts/get_data.py
+++ gendiff/scripts/get_data.py
@@ -8,8 +8,8 @@ def get_data(file_path):
         return json.load(f)
     
 
-data1 = get_data('tests/fixtures/file1.json')
-data2 = get_data('tests/fixtures/file2.json')
+data1 = get_data('gendiff/tests/fixtures/file1.json')
+data2 = get_data('gendiff/tests/fixtures/file2.json')
 
 print("Data first file:", data1)
 print("Data second file", data2)
